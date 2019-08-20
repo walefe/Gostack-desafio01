1dc0676c35cdc87742381d871fa8fdc5482e0b72
Walefe <walefedev@gmail.com>
2019-08-20 10:20:12 -0300

    Route put

--- index.js
+++ index.js
@@ -61,4 +61,15 @@ server.delete("/projects/:id", checkProjectId, (req, res) => {
   return res.json();
 });
 
+server.post("/projects/:id/tasks", checkProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.tasks.push(title);
+
+  return res.json(project);
+});
+
 server.listen(3000);
